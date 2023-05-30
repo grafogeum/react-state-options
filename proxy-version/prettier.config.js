6f3c9166c9d69f7e3e1709c958c7ecd807b85d50
michal <grafogeum@users.noreply.github.com>
2023-05-31 00:47:59 +0200

    init

---
+++ proxy-version/prettier.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+module.exports = {
+	trailingComma: 'es5',
+	tabWidth: 2,
+	tabSize: 2,
+	useTabs: true,
+	semi: true,
+	singleQuote: false,
+};
---
+++ proxy-version/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState, useRef } from 'react';
+
+function useProxy(obj) {
+  const [, setDummy] = useState(0)
+  const proxyRef = useRef(new Proxy(obj, {
+    // get(obj, key) {
+    //   return Reflect.get(...arguments)
+    // },
+    get() {
+      return Reflect.get(...arguments)
+    },
+    set() {
+      Reflect.set(...arguments)
+      setDummy(v => v + 1)
+      return true
+    }
+  }))
+  return proxyRef.current
+}
+
+function App() {
+
+  const user = useProxy({
+    name: 'Jan',
+    profession: 'Developer',
+    address: {
+      city: 'Szczecin'
+    }
+  })
+
+  const onChangeName = (e) => {
+    user.name = e.target.value
+  }
+  const onCityChange = (e) => {
+    user.address.city = e.target.value
+  }
+  return (
+    <>
+      <div>
+        <input type="text" value={user.name} onChange={onChangeName} />
+        <input type="text" value={user.address.city} onChange={onCityChange} />
+        <div>{user.name}</div>
+        <div>{user.profession}</div>
+        <div>{user.address.city}</div>
+      </div>
+    </>
+  )
+}
+
+export default App
